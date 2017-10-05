a85b5ad2301e3e5bf40378367d24a67142d1453c
Jan Drewniak <jan.drewniak@gmail.com>
2017-10-05 14:57:16 +0200

    typo in font loader path

--- js/fonts-loader.js
+++ js/fonts-loader.js
@@ -6,7 +6,7 @@ if ( document.head && 'Promise' in window ) {
 		html.classList.add( 'fonts-loaded' );
 	} else {
 		var script = document.createElement( 'script' );
-		script.src = MWF_FONT_PATH;
+		script.src = WMF_FONT_PATH;
 
 		script.onload = function () {
 			var sansSerif = new FontFaceObserver( 'Lato' );
